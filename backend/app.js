a2455b2b07555c0919411d3010593aec20e76fdc
user.email <roismuslim77@gmail.com>
2018-12-14 00:58:39 +0700

    heroku update

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/todoapp', ToDo);
 
-let port = 0077;
+// set CORS to allow access from any server
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", "PUT, PATCH, POST, DELETE, GET");
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
+let port = process.env.PORT||3000;
 
 app.listen(port, ()=>{
     console.log('Server is Up in port' + port)
